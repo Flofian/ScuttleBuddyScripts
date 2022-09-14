4209c7a9150f58214bb35cef505256f874b5ee30
Flofian <56082272+Flofian@users.noreply.github.com>
2022-09-14 20:07:36 +0200

    Add files via upload

---
+++ Minions.py
@@ -0,0 +1,45 @@
+# Importing LeagueReader for typing intellisense
+from resources import LeagueReader
+import math
+import os
+from scripts.helpers import Draw
+
+
+# Setup function | only runs once on script load
+def setup() -> dict:
+    scriptSettings: dict = {
+        "Minions": {
+            "displayName": "Lasthitable Minions",
+            "isEnabled": True
+        },
+    }
+
+    return scriptSettings
+
+
+# OnTick function | Runs every tick
+def on_tick(lReader: LeagueReader, pymeow, scriptSettings) -> None:
+    font = pymeow.font_init(20, "ComicSans")
+
+    team = lReader.localPlayer.teamId
+    ad = lReader.localPlayer.ad
+
+    minions: list = [minion for minion in lReader.minions if minion.teamId != team]
+    if scriptSettings['Minions']['isEnabled']:
+        find_minions(lReader, pymeow, minions, ad)
+
+
+def find_minions(lReader: LeagueReader, pymeow, minionlist, ad):
+    for minion in minionlist:
+        if minion.onScreen and minion.health < ad:
+            pos = minion.screenPos
+            x,y = pos["x"], pos["y"]
+            Draw.circle(
+                pymeow=pymeow,
+                x=x,
+                y=y+20,
+                radius=10,
+                color="green",
+                filled=True
+            )
+
